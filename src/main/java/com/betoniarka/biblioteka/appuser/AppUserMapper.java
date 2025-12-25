f6e22676213754e78bbc3d1e0a6e48732f8d8c05
lythx <lythxxx@gmail.com>
2025-12-25 14:22:30 +0100

    fix app user mapper

--- src/main/java/com/betoniarka/biblioteka/appuser/AppUserMapper.java
+++ src/main/java/com/betoniarka/biblioteka/appuser/AppUserMapper.java
@@ -9,21 +9,41 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface AppUserMapper {
 
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     AppUser toEntity(AppUserCreateDto source);
 
     @Mapping(target = "role", constant = "APP_USER")
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     AppUser toEntity(AppUserRegisterDto source);
 
     @Mapping(target = "password", source = "encryptedPassword")
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     void update(AppUserAdminUpdateDto source, @MappingTarget AppUser target, String encryptedPassword);
 
     @Mapping(target = "password", ignore = true)
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     void update(AppUserAdminUpdateDto source, @MappingTarget AppUser target);
 
     @Mapping(target = "password", source = "encryptedPassword")
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     void update(AppUserUpdateDto source, @MappingTarget AppUser target, String encryptedPassword);
 
     @Mapping(target = "password", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "borrows", ignore = true)
+    @Mapping(target = "queuedBooks", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
     void update(AppUserUpdateDto source, @MappingTarget AppUser target);
 
     AppUserResponseDto toDto(AppUser source);
